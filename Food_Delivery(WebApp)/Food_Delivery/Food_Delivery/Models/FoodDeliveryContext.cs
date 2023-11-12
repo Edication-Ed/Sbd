093eff32f37c24cb5f169ba34670e7e9b335ed3d
Dimka-Monkey <120569957+Dimka-Monkey@users.noreply.github.com>
2023-11-12 14:29:11 +0300

    12.11.2023 14:29

--- Food_Delivery(WebApp)/Food_Delivery/Food_Delivery/Models/FoodDeliveryContext.cs
+++ Food_Delivery(WebApp)/Food_Delivery/Food_Delivery/Models/FoodDeliveryContext.cs
@@ -35,7 +35,7 @@ public partial class FoodDeliveryContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=Food_Delivery;Username=postgres;Password=1234");
+        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=Food_Delivery;Username=postgres;Password=Dima2023");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
